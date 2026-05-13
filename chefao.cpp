9fdad00a020ae5d3b9f0a267ce8bfd5ad4022ed8
Raphael Geraldine <rapha.geraldine@gmail.com>
2026-05-13 15:35:26 -0300

    testing commit 2

---
+++ chefao.cpp
@@ -0,0 +1,13 @@
+namespace Personagens
+{
+    class Tie
+    {
+        private:
+            short int vida;
+            short int forca;
+
+        public:
+            Tie (short int v=100, short int f = 80);
+            ~Tie ();
+    };
+}
