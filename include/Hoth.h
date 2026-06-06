93c83c87dada3deba841aa73c396d68f2332fb4f
Raphael Geraldine <rapha.geraldine@gmail.com>
2026-06-06 16:03:17 -0300

    Fase 2 is rising

--- include/Hoth.h
+++ include/Hoth.h
@@ -1,18 +1,42 @@
 #pragma once
 
+//#include "../include/AT_ST.h"
+#include "../include/Lista.h"
+#include "../include/ListaEntidades.h"
+#include "../include/Inimigo.h"
+#include "../include/Obstaculo.h"
+#include "../include/Gerenciador_Colisoes.h"
+#include "../include/Ente.h"
+#include "../include/Fase.h"
+
 namespace TrabalhoJogo
 {
-    namespace Fase
+    namespace Fases
     {
         class Hoth : public Fase
         {
             private:
+                const int minChefoes;
                 const int maxChefoes;
+                const int minObstDificil;
+                const int maxObstDificil;
+                sf::Texture texturaFundo; 
+                sf::Sprite fundo;
 
-            protected:
-                void criarChefoes();
-                void criarObstaculosMedios();
+            private:
+                void incluirEntidade(Entidades::Entidade* pE);
+                void incluirJogador(Entidades::Personagens::Jogador* pJ);
+                void incluirInimigo(Entidades::Personagens::Inimigo* pI);
+                void incluirObstaculo(TrabalhoJogo::Entidades::Obstaculos::Obstaculo* pO);
+                void criarInimigos(); //Chefoes: Dificil
+                void criarObstaculos(); //Dificil
                 void criarProjeteis();
+
+            public:
+                Hoth(Entidades::Personagens::Jogador* pJ1 = nullptr, 
+                     Entidades::Personagens::Jogador* pJ2 = nullptr);
+                ~Hoth();
+                sf::Sprite getDrawData();
 
         };
     }
---
+++ src/Hoth.cpp
@@ -0,0 +1,175 @@
+#define HOTHPNG "../assets/images/Hoth.png"
+
+//#include "../include/Gelo.h"
+//#include "../include/AT_ST.h"
+#include "../include/Entidade.h"
+#include "../include/Fase.h"
+#include "../include/Jogador.h"
+#include "../include/Inimigo.h"
+#include "../include/Obstaculo.h"
+#include "../include/Gerenciador_Grafico.h"
+#include "../include/Hoth.h"
+using namespace TrabalhoJogo;
+using namespace Listas;
+using namespace Entidades;
+using namespace Obstaculos;
+using namespace Fases;
+using namespace Gerenciadores;
+
+Hoth::Hoth(Jogador* pJ1, Jogador* pJ2): 
+    Fase(pJ1, pJ2),
+    minChefoes(3),
+    maxChefoes(7),
+    minObstDificil(3),
+    maxObstDificil(5)
+{   
+    sf::Texture* pTexturaFundo = Gerenciador_Grafico::getGerenciadorGrafico()->carregarTextura(HOTHPNG);
+        
+    if (pTexturaFundo == 0)
+        cerr << "Erro de carregamento do Plano de Fundo de Hoth" << endl;
+
+    else
+        fundo.setTexture(*pTexturaFundo); 
+    
+    inicializar(pJ1,pJ2);
+    criarObstaculos();
+    criarInimigos();
+}
+
+Hoth::~Hoth()
+{
+
+}
+
+void Hoth::criarInimigos()
+{
+    /*
+    int qntd = rand()%(maxChefoes-minChefoes+1) + minChefoes;
+    for (int i = 0; i < qntd; i++)
+    {
+        AT_ST* pAT = new AT_ST();
+        if (pAT == nullptr)
+            cerr << "Tentativa de incluir AT-ST nula na lista de entidades." << endl;
+        else
+            incluirEntidade(static_cast<Inimigo*>(pAT)); 
+    }
+    */
+}
+
+void Hoth::criarObstaculos()
+{
+    /*
+    int qntd = rand()%(maxObstDificil-minObstDificil+1) + minObstDificil;
+    for (int i = 0; i < qntd; i++)
+    {
+        Gelo* pGelo = new Gelo();
+        if (pGelo == nullptr)
+            cerr << "Tentativa de incluir gelo nula na lista de entidades." << endl;
+        else   
+            incluirEntidade(static_cast<Obstaculo*>(pGelo));
+    }
+    */
+}
+
+void Hoth::criarProjeteis()
+{
+
+}
+
+void Hoth::incluirEntidade(Entidade* pE) 
+{
+    if (pE == nullptr)
+    {
+        std::cerr << "Erro: tentativa de incluir entidade nula na fase." << std::endl;
+        return;
+    }
+
+    // Inspiração no menu do Sistema Acadêmico, visto em sala,
+    // durante aula do Prof. Dr. Jean Simão.
+    switch (pE->getID())
+    {
+        // Depois restringir ao usar inimigos e afins.
+        case jogador:
+        {
+            incluirJogador(static_cast<Jogador*>(pE));
+            break;
+        }
+
+        case inimigo:
+        {
+            incluirInimigo(static_cast<Inimigo*>(pE));
+            break;
+        }
+
+        case obstaculo:
+        {
+            incluirObstaculo(static_cast<Obstaculo*>(pE));
+            break;
+        }
+
+        default:
+        {
+            listaEntidades.incluir(pE);
+            break;
+        }
+    }
+}
+
+void Hoth::incluirJogador(Jogador* pJ)
+{
+    if (pJ == nullptr)
+    {
+        cerr << "Erro: tentativa de incluir jogador nulo na fase." << endl;
+        return;
+    }
+
+    if (listaJogadores.getTamanho() >= 2)
+    {
+        cerr << "Erro: a fase suporta no maximo 2 jogadores." << endl;
+        return;
+    }
+
+    listaEntidades.incluir(pJ);
+    listaJogadores.incluir(pJ);
+}
+
+void Hoth::incluirInimigo(Inimigo* pI)
+{
+    if (pI == nullptr)
+    {
+        cerr << "Erro: tentativa de incluir inimigo nulo na fase." << endl;
+        return;
+    }
+
+    if (listaInimigos.getTamanho() >= 45)
+    {
+        cerr << "Erro: a fase suporta no maximo 45 inimigos." << endl;
+        return;
+    }
+
+    listaEntidades.incluir(static_cast<Entidade*>(pI));
+    listaInimigos.incluir(pI);
+}
+
+void Hoth::incluirObstaculo(Obstaculo* pO)
+{
+    if (pO == nullptr)
+    {
+        cerr << "Erro: tentativa de incluir obstaculo nulo na fase." << endl;
+        return;
+    }
+
+    if (listaObstaculos.getTamanho() >= 45)
+    {
+        cerr << "Erro: a fase suporta no maximo 45 obstaculos." << endl;
+        return;
+    }
+
+    listaEntidades.incluir(static_cast<Entidade*>(pO));
+    listaObstaculos.incluir(pO);
+}
+
+sf::Sprite Hoth::getDrawData()
+{
+    return fundo;
+}
